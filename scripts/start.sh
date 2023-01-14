098051f39efab03969b637f93d70ab4ef8d15f22
SangwonYoon <38793142+SangwonYoon@users.noreply.github.com>
2023-01-14 19:56:03 +0900

    CI CD 설정

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,5 +3,8 @@
 PROJECT_ROOT="/home/ubuntu/app"
 TARGET_DIR="$PROJECT_ROOT/Foodle-data/foodle"
 
+# dependency 설치
+pip install -r requirements.txt
+
 # 서버 실행
 nohup python $TARGET_DIR/manage.py runserver 0.0.0.0:8000 &
